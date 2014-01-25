e3a727ba88067169ab1829a0043e6adad36f33a8
Ujwal Patel <ujwalp@codeaurora.org>
2014-01-24 16:34:53 -0800

    msm: mdss: enable under-run interrupt for secondary ctl
    In split-display use-cases two ctl paths are active but one acts as
    master and other as secondary. Currently under-run interrupt is only
    enabled for master ctl. Enable under-run interrupt for secondary ctl
    for seamless tracking.
    
    Change-Id: I849419c4077fed384431079952027149c31b33fc
    Signed-off-by: Ujwal Patel <ujwalp@codeaurora.org>
    Signed-off-by: Jayant Shekhar <jshekhar@codeaurora.org>

--- drivers/video/msm/mdss/mdss_mdp_intf_video.c
+++ drivers/video/msm/mdss/mdss_mdp_intf_video.c
@@ -300,6 +300,7 @@ static int mdss_mdp_video_stop(struct mdss_mdp_ctl *ctl)
 {
 	struct mdss_mdp_video_ctx *ctx;
 	struct mdss_mdp_vsync_handler *tmp, *handle;
+	struct mdss_mdp_ctl *sctl;
 	int rc;
 	u32 frame_rate = 0;
 
@@ -339,6 +340,10 @@ static int mdss_mdp_video_stop(struct mdss_mdp_ctl *ctl)
 
 		mdss_mdp_irq_disable(MDSS_MDP_IRQ_INTF_UNDER_RUN,
 			ctl->intf_num);
+		sctl = mdss_mdp_get_split_ctl(ctl);
+		if (sctl)
+			mdss_mdp_irq_disable(MDSS_MDP_IRQ_INTF_UNDER_RUN,
+				sctl->intf_num);
 	}
 
 	list_for_each_entry_safe(handle, tmp, &ctx->vsync_handlers, list)
@@ -659,6 +664,7 @@ static int mdss_mdp_video_config_fps(struct mdss_mdp_ctl *ctl,
 static int mdss_mdp_video_display(struct mdss_mdp_ctl *ctl, void *arg)
 {
 	struct mdss_mdp_video_ctx *ctx;
+	struct mdss_mdp_ctl *sctl;
 	int rc;
 
 	pr_debug("kickoff ctl=%d\n", ctl->num);
@@ -692,6 +698,11 @@ static int mdss_mdp_video_display(struct mdss_mdp_ctl *ctl, void *arg)
 		mdss_mdp_clk_ctrl(MDP_BLOCK_POWER_ON, false);
 
 		mdss_mdp_irq_enable(MDSS_MDP_IRQ_INTF_UNDER_RUN, ctl->intf_num);
+		sctl = mdss_mdp_get_split_ctl(ctl);
+		if (sctl)
+			mdss_mdp_irq_enable(MDSS_MDP_IRQ_INTF_UNDER_RUN,
+				sctl->intf_num);
+
 		mdp_video_write(ctx, MDSS_MDP_REG_INTF_TIMING_ENGINE_EN, 1);
 		wmb();
 
