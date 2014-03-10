de660ea67af62bcfe383ac14036640bfa29170f8
Naseer Ahmed <naseer@codeaurora.org>
2014-03-10 14:11:41 -0400

    mdss: Notify userspace of changes to blank status
    Allow userspace to poll on the blank event to avoid triggering
    unnecessary updates, such as when shutting down the device.
    
    CRs-Fixed: 627814
    Change-Id: I36bb7845edba14ba0b0ddbe43eaf3d386af2d5f1
    Signed-off-by: Naseer Ahmed <naseer@codeaurora.org>

--- drivers/video/msm/mdss/mdss_fb.c
+++ drivers/video/msm/mdss/mdss_fb.c
@@ -1027,6 +1027,8 @@ int mdss_fb_blank_sub(int blank_mode, struct fb_info *info, int op_enable)
 		}
 		break;
 	}
+	/* Notify listeners */
+	sysfs_notify(&mfd->fbi->dev->kobj, NULL, "show_blank_event");
 
 	if (!mfd->quickdraw_in_progress) {
 		/* Notify listeners */
